22bf51ab490297125e36a1dde0f2c098648fc0f4
OrayKT <oray.kt@gmail.com>
2019-03-27 20:37:23 +0100

    helper/findResult

--- app.js
+++ app.js
@@ -2,25 +2,13 @@ const _ = require('lodash');
 const yargs = require('yargs');
 const args = yargs.argv;
 const findMultiple = require('./helper/findMultiple');
-const checkInput = require('./helper/checkInput');
-
+const findResult = require('./helper/findResult');
 const availableNodes = [100, 50, 20, 10];
 
 try {
     let userInput = args._[0];
     userInput = !userInput ? null : userInput;
-    const resultArr = [];
-    checkInput(userInput);
-    availableNodes.forEach((node, index) => {
-        let tempDiv = findMultiple(index, userInput, availableNodes);
-        if (tempDiv) {
-            userInput -= tempDiv * node;
-            do {
-                resultArr.push(node);
-                tempDiv--;
-            } while (tempDiv !== 0)
-        }
-    });
+    const resultArr = findResult(availableNodes, userInput);
     console.log("Result: ", resultArr);
 
 } catch (exception) {
