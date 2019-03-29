ebaaca7b8b22b331b657a382d346d73d522d9d4d
oraykt <oraykt@gmail.com>
2019-03-29 01:42:03 +0100

    Folders refactored

--- utils/checkInput.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable no-throw-literal */
-module.exports = (userInput) => {
-  if (!userInput) { return }
-  if (userInput < 0 || typeof (userInput) !== 'number') { throw 'InvalidArgumentException' }
-}
