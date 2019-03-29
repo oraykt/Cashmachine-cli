38ed2c17a6772c3986363daf8118e736af336893
oraykt <oraykt@gmail.com>
2019-03-29 01:51:10 +0100

    0 has an output

--- src/services/calculateAmount.js
+++ src/services/calculateAmount.js
@@ -2,15 +2,19 @@ const output = require('../utils/output')
 module.exports = (resultArray) => {
   let amount = 1
   // TODO output will declare with a global variable
-  resultArray.forEach((value, index) => {
-    if (resultArray[index + 1]) {
-      if (value === resultArray[index + 1]) {
-        amount++
+  if (resultArray.length === 0) {
+    console.log('Empty') // TODO
+  } else {
+    resultArray.forEach((value, index) => {
+      if (resultArray[index + 1]) {
+        if (value === resultArray[index + 1]) {
+          amount++
+        } else {
+          console.log(output(value, amount))
+        }
       } else {
         console.log(output(value, amount))
       }
-    } else {
-      console.log(output(value, amount))
-    }
-  })
+    })
+  }
 }
