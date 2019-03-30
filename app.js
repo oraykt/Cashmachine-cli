8ca6e0b608618afdcd4e56561c104402ef37b3e8
oraykt <oraykt@gmail.com>
2019-03-30 19:44:11 +0100

    app.js /src

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-throw-literal */
 const yargs = require('yargs')
 const args = yargs.argv
-const cashMachineService = require('./services/CashMachine')
+const cashMachineService = require('./src/services/CashMachine')
 
 cashMachineService(args._[0])
